274075dd859815bbcdcaf8857597854226a5460a
lenovo <lenovo@LAPTOP-2NKIQS9D.station>
2021-04-28 17:00:57 +0200

    commit

--- src/main/java/it/polito/tdp/poweroutages/model/Model.java
+++ src/main/java/it/polito/tdp/poweroutages/model/Model.java
@@ -6,7 +6,7 @@ import java.util.List;
 import it.polito.tdp.poweroutages.DAO.PowerOutageDAO;
 
 public class Model {
-	
+	 
 	PowerOutageDAO podao;
 	int sommaClientiMigliore;
 	List<PowerOutage> soluzioneMigliore;
@@ -26,41 +26,53 @@ public class Model {
 	public List<PowerOutage> trovaSequenza(int nerc_id, int maxOre, int maxAnni){
 		
 		sommaClientiMigliore = 0;
-		itera(0, 0, new ArrayList<PowerOutage>(), this.getPowerList(nerc_id), 0, maxOre, 0, maxAnni);
+		itera(0, 0, new ArrayList<PowerOutage>(), this.getPowerList(nerc_id), 0, maxOre, 0.0, maxAnni);
 		
 		return soluzioneMigliore;
 	}
-	
-	public void itera(int livello, int indice, List<PowerOutage> parziale, List<PowerOutage> powerList, int sommaClienti, int maxOre, double sommaOre, int maxAnni) {
+	 
+	public void itera(int livello, int indice, List<PowerOutage> parziale, List<PowerOutage> powerList, int sommaClienti, 
+			int maxOre, double sommaOre, int maxAnni) {
 		
-		if(parziale.size()>0 && (livello==powerList.size() || (sommaOre+powerList.get(indice).getHours())>maxOre || 
+		System.out.println(livello+"\t"+indice+"\t"+parziale.size());
+		
+		if(indice==powerList.size()){
+			itera(livello+1, livello+1, new ArrayList<PowerOutage>(), powerList, 0, maxOre, 0.0, maxAnni);
+			
+			return;
+		}
+		
+		if(parziale.size()>0 && (livello==100 || (sommaOre+powerList.get(indice).getHours())>maxOre || 
 				(parziale.get(0).getYear()-powerList.get(indice).getYear())>maxAnni)) {
 				
 			if(sommaClienti>sommaClientiMigliore) {
 				sommaClientiMigliore=sommaClienti;
 				soluzioneMigliore = new ArrayList<>(parziale);
+				
+				//System.out.println(soluzioneMigliore.get(soluzioneMigliore.size()-1));
+				
 				return;
 			}
 			
+			return;
+			
 		}
 		
+		for(int i=indice; i<powerList.size(); i++) {
+			
+			parziale.add(powerList.get(i));
+			
+			itera(livello, i+1, parziale, powerList, sommaClienti+powerList.get(i).getCustomers_affected(), maxOre, 
+					sommaOre+powerList.get(i).getHours(), maxAnni);
+			
+			parziale.remove(powerList.get(i));
 		
-		if(indice<powerList.size()) {
-			
-			parziale.add(powerList.get(indice));
-			
-			itera(livello, indice+1, parziale, powerList, sommaClienti+powerList.get(indice).getCustomers_affected(), maxOre, 
-					sommaOre+powerList.get(indice).getHours(), maxAnni);
-			
-			parziale.remove(powerList.get(indice));
-		
-		}else{
-			
-			itera(livello+1, livello+1, new ArrayList<PowerOutage>(), powerList, 0, maxOre, 0, maxAnni);
 		}
-		
-	}
+	
+	
+	}	
+}
 	
 	
 
-}
+
--- src/main/java/it/polito/tdp/poweroutages/model/TestModel.java
+++ src/main/java/it/polito/tdp/poweroutages/model/TestModel.java
@@ -3,7 +3,7 @@ package it.polito.tdp.poweroutages.model;
 public class TestModel {
 
 	public static void main(String[] args) {
-		
+		 
 		Model model = new Model();
 		//System.out.println(model.getNercList());
 		//System.out.println(model.trovaSequenza(8, 100, 2));
